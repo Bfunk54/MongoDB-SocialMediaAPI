6645fdc64f38d2a545a3c0606b8eee8b05017d8f
Bfunk54 <ben_fein@icloud.com>
2022-11-30 14:41:53 -0500

    added the delete function to the user controller, definitely not done, it is a start!

--- controllers/userController.js
+++ controllers/userController.js
@@ -46,3 +46,28 @@ module.exports = {
       .then((user) => res.json(user))
       .catch((err) => res.status(500).json(err));
   },
+
+  // Delete a user and remove them from the course
+  deleteUser(req, res) {
+    User.findOneAndRemove({ _id: req.params.userId })
+      .then((user) =>
+        !user
+          ? res.status(404).json({ message: 'This user does not exist' })
+          : Thought.findOneAndUpdate(
+              { user: req.params.userId },
+              { $pull: { user: req.params.userId } },
+              { new: true }
+            )
+      )
+      .then((thought) =>
+        !thought
+          ? res.status(404).json({
+              message: 'User deleted, but no thoughts found',
+            })
+          : res.json({ message: 'User successfully deleted' })
+      )
+      .catch((err) => {
+        console.log(err);
+        res.status(500).json(err);
+      });
+  },
