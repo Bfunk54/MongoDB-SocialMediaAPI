51972d9730b8626f8596a7c592fd69735a90460a
Bfunk54 <ben_fein@icloud.com>
2022-11-30 13:12:06 -0500

    finished the get request for all users

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,2 +1,18 @@
 const { ObjectId } = require('mongoose').Types;
 const { User , Thought } = require('../models');
+
+module.exports = {
+    // Route to get all users
+    getUsers(req, res) {
+        User.find()
+          .then(async (users) => {
+            const userObj = {
+              users,
+            };
+            return res.json(userObj);
+          })
+          .catch((err) => {
+            console.log(err);
+            return res.status(500).json(err);
+          });
+      },
