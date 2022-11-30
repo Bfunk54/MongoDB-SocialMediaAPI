8524809d5f082be54584b54a8aae27489e73999f
Bfunk54 <ben_fein@icloud.com>
2022-11-30 14:37:57 -0500

    added a create user example

--- controllers/userController.js
+++ controllers/userController.js
@@ -37,6 +37,11 @@ module.exports = {
 
    // route to create a new user
    createUser(req, res) {
+       /* example data
+  {
+   "username": "lernantino",
+   "email": "lernantino@gmail.com"
+  } */
     User.create(req.body)
       .then((user) => res.json(user))
       .catch((err) => res.status(500).json(err));
