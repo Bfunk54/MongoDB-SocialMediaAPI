a8b531b28f415f95386f474a68db5e511c684bac
Bfunk54 <ben_fein@icloud.com>
2022-11-30 21:48:29 -0500

    added the route to add a reaction to a thought in the thought routes

--- routes/api/thoughtRoutes.js
+++ routes/api/thoughtRoutes.js
@@ -1,20 +1,23 @@
-const router = require('express').Router();
+const router = require("express").Router();
 const {
   getThoughts,
   getSingleThought,
   createThought,
   updateThought,
   deleteThought,
-} = require('../../controllers/thoughtController.js');
+} = require("../../controllers/thoughtController.js");
 
 // route to get all thoughts and create a new thought
-router.route('/').get(getThoughts).post(createThought);
+router.route("/").get(getThoughts).post(createThought);
 
 // route to get a single thought, update a single thought and delete a thought
 router
-  .route('/:thoughtId')
+  .route("/:thoughtId")
   .get(getSingleThought)
   .put(updateThought)
   .delete(deleteThought);
+
+// route to add a reaction to a thought
+router.route("/:thoughtId/reactions").post(addReaction);
 
 module.exports = router;
