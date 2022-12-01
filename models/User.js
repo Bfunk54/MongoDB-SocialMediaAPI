a5bc4a2600641caa91b5f22586d162b8322703de
Bfunk54 <ben_fein@icloud.com>
2022-12-01 12:33:59 -0500

    added the virtual for friend count for the users

--- models/User.js
+++ models/User.js
@@ -41,6 +41,10 @@ const userSchema = new Schema(
   }
 );
 
+userSchema.virtual("friendCount").get(function () {
+  return this.friends.length;
+});
+
 const User = model("user", userSchema);
 
 module.exports = User;
