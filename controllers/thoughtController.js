629111cd5bf5b1ba537cf17d06b1aa22a633abd8
Bfunk54 <ben_fein@icloud.com>
2022-11-30 20:43:12 -0500

    finished the get all thoughts function in the thought controller

---
+++ controllers/thoughtController.js
@@ -0,0 +1,90 @@
+const { ObjectId } = require("mongoose").Types;
+const { User, Thought } = require("../models");
+
+module.exports = {
+  // Get all thoughts
+  getThoughts(req, res) {
+    Thought.find()
+      .then(async (thoughts) => {
+        const thoughtsObj = {
+          thoughts,
+        };
+        return res.json(thoughtsObj);
+      })
+      .catch((err) => {
+        console.log(err);
+        return res.status(500).json(err);
+      });
+  },
+
+  // Get a single user by id
+  getSingleUser(req, res) {
+    User.findOne({ _id: req.params.userId, include: Thought })
+      .select("-__v")
+      .then(async (user) =>
+        !user
+          ? res.status(404).json({ message: "No user with that ID" })
+          : res.json({
+              user,
+              friends: await friend(req.params.userId),
+            })
+      )
+      .catch((err) => {
+        console.log(err);
+        return res.status(500).json(err);
+      });
+  },
+
+  // Create a new user
+  createUser(req, res) {
+    /* example data
+  {
+   "username": "lernantino",
+   "email": "lernantino@gmail.com"
+  } */
+    User.create(req.body)
+      .then((user) => res.json(user))
+      .catch((err) => res.status(500).json(err));
+  },
+
+  // Delete a user and remove them from the course
+  deleteUser(req, res) {
+    User.findOneAndRemove({ _id: req.params.userId })
+      .then((user) =>
+        !user
+          ? res.status(404).json({ message: "This user does not exist" })
+          : Thought.findAllAndDelete(
+              { user: req.params.userId },
+              { $pull: { user: req.params.userId } },
+              { new: true }
+            )
+      )
+      .then((thought) =>
+        !thought
+          ? res.status(404).json({
+              message: "User deleted, but no thoughts found",
+            })
+          : res.json({
+              message: "User and all thoughts have been successfully deleted",
+            })
+      )
+      .catch((err) => {
+        console.log(err);
+        res.status(500).json(err);
+      });
+  },
+
+  // Update a user
+  updateUser(req, res) {
+    User.findOneAndUpdate({ _id: req.params.userId }, req.body, { new: true })
+      .then((user) =>
+        !user
+          ? res.status(404).json({ message: "This user does not exist" })
+          : res.json({ message: "User successfully updated" })
+      )
+      .catch((err) => {
+        console.log(err);
+        res.status(500).json(err);
+      });
+  },
+};
